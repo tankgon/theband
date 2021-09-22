018bf9165535966344e00a68ec00888b333b180b
tankgon <91188568+tankgon@users.noreply.github.com>
2021-09-22 17:25:33 +0700

    Release suntech frontend final project

---
+++ javascript/java.js
@@ -0,0 +1,56 @@
+// // cach dat ten bien
+
+// var fullname = 'Nguyễn Phúc Thanh',
+//     age = 20, 
+//     work ='Huflit';
+
+// alert(fullname);
+// alert(age);
+// alert(work);
+
+
+
+// // thay thế đồ vật khác
+
+// var fullName; // tạo chiếc hộp
+
+// fullName = 'Sơn Đặng'; // cho đồ vật vào hộp
+
+// fullName = 'Nguyễn Văn A'; // thay thế đồ vật khác
+
+// alert(fullName); // Nguyễn Văn A
+
+
+
+// // sao chep gia tri
+// var currentCourse = 'Javascript';
+
+// var newCourse;
+
+// // copy giá trị 'Javascript' từ biến
+// // 'currentCourse' sang biến 'newCourse'
+// newCourse = currentCourse;
+
+// // bây giờ, biến 'newCourse' và 'currentCourse'
+// // đều có giá trị là 'Javascript'
+// alert(currentCourse); // Javascript
+// alert(newCourse); // Javascript
+
+// var fullName =('Nguyen Phuc Thanh');
+// console.log(fullName);
+
+
+
+// var a = 1+2;
+// console.log(a);
+
+var a=1;b=3
+// if (a<b) {
+    // console.log("dung");
+// }
+
+// if(a>0  b>0){
+    // console.log('dung roi dm')
+// }
+
+
